bb3b1e006c5fe7c633ca07b56f0005609f0f968d
Phiêu Lê Văn <levanphieu99@gmail.com>
2021-07-28 09:24:11 +0700

    fix

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -11,19 +11,21 @@ module.exports = {
         try {
             let { content } = req.body;
             let { receiver_id } = req.params;
-            let error,conversation_id;
+            let error, conversation_id;
 
-            [error,conversation_id] = await to(conversationService.checkExistsConservationID(receiver_id));
+            [error, conversation_id] = await to(conversationService.checkExistsConservationID(receiver_id));
 
-            if(error){
+            if (error) {
                 [error, conversation_id] = await to(conversationService.checkExistsConservationMember(req.userId, receiver_id));
 
-                if(error){
+                if (error) {
                     req.body.members = [
-                        req.userId,  
+                        req.userId,
                         receiver_id
                     ];
                     req.body.last_message = content;
+                    req.body.created_at = Date.now();
+                    req.body.last_update = Date.now();
                     let conversation = await conversationService.create(req.body);
 
                     let body = {
@@ -35,9 +37,9 @@ module.exports = {
                     };
                     await messsageService.create(body);
                     sendReportToUser(receiver_id, CONST.EVENT.PERSON_MESSAGE, { message: body }) // gửi tới thành viên
-                    return ApiResponse(res, 200, CONST.MESSAGE.SUCCESS,{}, version);
+                    return ApiResponse(res, 200, CONST.MESSAGE.SUCCESS, {}, version);
                 }
-                else{
+                else {
                     let body = {
                         conversation_id,
                         sender_id: req.userId,
@@ -56,7 +58,7 @@ module.exports = {
                     sendReportToUser(receiver_id, CONST.EVENT.PERSON_MESSAGE, { message: body }) // gửi tới thành viên
                     return ApiResponse(res, 200, CONST.MESSAGE.SUCCESS, {}, version);
                 }
-            }else{
+            } else {
                 let body = {
                     conversation_id,
                     sender_id: req.userId,
@@ -72,10 +74,10 @@ module.exports = {
                     conversationService.update(conversation_id, body_update),
                     messsageService.create(body)
                 ]);
-                let conversation =await conversationService.getOneById(receiver_id);
+                let conversation = await conversationService.getOneById(receiver_id);
                 for (let i = 0; i < conversation.members.length; i++) {
                     let receiver_id = conversation.members[i];
-                    if(receiver_id != req.userId){
+                    if (receiver_id != req.userId) {
                         sendReportToUser(receiver_id, CONST.EVENT.PERSON_MESSAGE, { message: body }) // gửi tới tất cả thành viên
                     }
                 }
@@ -89,23 +91,23 @@ module.exports = {
         try {
             let page_index = Number(req.query.page_index) ? Number(req.query.page_index) : 1;
             let page_size = Number(req.query.page_size) ? Number(req.query.page_size) : 10;
-            
+
             let receiver_id = req.params.id;
 
             let error, conversation_id;
 
             [error, conversation_id] = await to(conversationService.checkExistsConservationID(receiver_id));
 
-            if(error){
+            if (error) {
                 [error, conversation_id] = await to(conversationService.checkExistsConservationMember(req.userId, receiver_id));
 
-                if(error){
+                if (error) {
                     return ApiResponse(res, 200, CONST.MESSAGE.SUCCESS, [], version);
-                }else{
+                } else {
                     let ans = await messsageService.getAllConversation(conversation_id, page_index, page_size);
                     return ApiResponse(res, 200, CONST.MESSAGE.SUCCESS, ans, version);
                 }
-            }else{
+            } else {
                 let ans = await messsageService.getAllConversation(conversation_id, page_index, page_size);
                 return ApiResponse(res, 200, CONST.MESSAGE.SUCCESS, ans, version);
             }
