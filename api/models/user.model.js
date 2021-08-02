591ba5a148f8d4b6ffb8bc0647257a5007cfb7b8
Phiêu Lê Văn <levanphieu99@gmail.com>
2021-08-02 09:24:27 +0700

    fix

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,10 +4,10 @@ const { DATABASE_NAME } = require('../../constants/database');
 let Schema = new mongoose.Schema({
     avatar: {
         type: String,
-        default: ""
+        default: "https://st4.depositphotos.com/4329009/19956/v/600/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg"
     },
     fullname: {
-        require:true,
+        require: true,
         type: String,
         default: ""
     },
@@ -18,39 +18,39 @@ let Schema = new mongoose.Schema({
     sex: {
         type: Number,
         default: 0,
-        enum:[0,1]
+        enum: [0, 1]
     },
     email: {
-        require:true,
-        type: String, 
-        default: "" 
-    },
-    token_verify:{
-        type: String, 
+        require: true,
+        type: String,
         default: ""
     },
-    created_at: { 
-        type: Number, 
+    token_verify: {
+        type: String,
+        default: ""
+    },
+    created_at: {
+        type: Number,
         default: Date.now()
     },
-    expired_at: { 
-        type: Number, 
-        default: 0 
+    expired_at: {
+        type: Number,
+        default: 0
     },
-    last_online: { 
-        type: Number, 
+    last_online: {
+        type: Number,
         default: Date.now()
     },
-    password: { 
+    password: {
         type: String,
         required: true,
-        default: "" 
+        default: ""
     },
-    status: { 
+    status: {
         type: Number,
-        default: 0 
+        default: 0
     },
-    
+
 });
 
 mongoose.model(DATABASE_NAME.USER, Schema)
